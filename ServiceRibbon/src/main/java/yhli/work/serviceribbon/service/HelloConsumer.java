913249a5c4f2ca0c336501e7384cd191f6d1aa7f
liyuehong <yhli394@163.com>
2023-06-27 21:30:27 +0800

    feat: 解决servlet-api版本过低的问题

---
+++ ServiceRibbon/src/main/java/yhli/work/serviceribbon/service/HelloConsumer.java
@@ -0,0 +1,19 @@
+package yhli.work.serviceribbon.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@Service
+public class HelloConsumer {
+
+
+    @Resource
+    RestTemplate restTemplate;
+
+    public String helloService(String name){
+        return restTemplate.getForObject("http://clientOne/begin?name="+name, String.class);
+    }
+
+}
---
+++ ServiceRibbon/src/test/java/yhli/work/serviceribbon/ServiceRibbonApplicationTests.java
@@ -0,0 +1,13 @@
+package yhli.work.serviceribbon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRibbonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
