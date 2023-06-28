506c2f3a0929f931e3f4d326ce854aa0d1e5e1ba
liyuehong <yhli394@163.com>
2023-06-28 17:00:06 +0800

    feat: 学习通过Feign消费服务

---
+++ tutorial3/ServiceFeign/src/main/java/yhli/work/servicefeign/ServletInitializer.java
@@ -0,0 +1,13 @@
+package yhli.work.servicefeign;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ServiceFeignApplication.class);
+    }
+
+}
