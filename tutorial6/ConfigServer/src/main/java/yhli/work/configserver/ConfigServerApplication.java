b08e2bb712fcaa2c41ae1339201ef327d78ee598
liyuehong <yhli394@163.com>
2023-06-30 16:52:05 +0800

    fix: 移动configcenter的位置

--- tutorial6/ConfigServer/src/main/java/yhli/work/configserver/ConfigServerApplication.java
+++ tutorial6/ConfigServer/src/main/java/yhli/work/configserver/ConfigServerApplication.java
@@ -2,6 +2,7 @@ package yhli.work.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
