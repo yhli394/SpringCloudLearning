a3ea9cf6014b353bc399f421a28a44e1b0b4eab7
liyuehong <yhli394@163.com>
2023-07-02 17:17:37 +0800

    fix: Web资源获取错误（main分支是默认分支，缺少spring.cloud.config.name配置）

---
+++ tutorial6/ConfigClient/src/main/java/yhli/work/configclient/ConfigClientApplication.java
@@ -0,0 +1,25 @@
+package yhli.work.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @Value("${my.name}")
+    String name;
+
+    @RequestMapping("/hi")
+    public String hi(){
+        return name;
+    }
+
+}
---
+++ tutorial6/ConfigClient/src/test/java/yhli/work/configclient/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package yhli.work.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
